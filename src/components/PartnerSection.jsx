7fcc48f726e204bf433af68c5de8f3a13c6d4b2a
Falase Femi <femifalase228@gmail.com>
2022-11-30 04:40:14 +0100

    partnersection done

--- src/components/PartnerSection.jsx
+++ src/components/PartnerSection.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import light from "../imgs1/light-square.png";
 
 const PartnerSection = () => {
   return (
-    <div>PartnerSection</div>
-  )
-}
+    <section className="partner-section">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <h1 className="partner-header text-capitalize text-center mb-4">
+              Trusted And Supported By
+            </h1>
+            <div className="row row-cols-2 col-md-12 me-2 ailgn-items-center justify-content-center row-cols-lg-4 g-3 g-lg-12">
+              <div className="col-md-3">
+                <div className="border">
+                  <h3>LOADING...</h3>
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="border">
+                  <img src={light} className="img-fluid" alt="" />
+                </div>
+              </div>
+              <div className="col-md-3">
+                <div className="border">
+                  <h3>LOADING...</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default PartnerSection
+export default PartnerSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Navbar, Hero, WhySection, ChooseSection, LabourSection, RangeSection } from '../components'
+import { Navbar, Hero, WhySection, ChooseSection, LabourSection, RangeSection, PartnerSection } from '../components'
 
 const Home = () => {
   return (
@@ -10,6 +10,7 @@ const Home = () => {
     <ChooseSection />
     <LabourSection />
     <RangeSection />
+    <PartnerSection />
     </>
   )
 }
